4f2a6e68229472168adf9091a6a45f78fa599ca3
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-08-12 22:19:23 -0300

    Fim da atualização para PC`s

--- src/Componentes/Projetos/index.jsx
+++ src/Componentes/Projetos/index.jsx
@@ -1,8 +1,23 @@
+import CardsProject from "./CardsProjects";
+import sAcessi from "../../assets/Projetos/siteAcessibilidade.png"
+import apiClima from "../../assets/Projetos/apiClima.png"
+import quizImg from "../../assets/Projetos/quiz.png"
+import cardapio from "../../assets/Projetos/cardapio.png"
+import gerador from "../../assets/Projetos/geradorPass.png"
+import burger from "../../assets/Projetos/burger.png"
+
+import './Projetos.css'
 export default function Projetos() {
     return (
-      <>
-     <span>UUUUUUUUUUUUaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaUUUUUUUUUUUUUUUUUUUU</span>
-      </>
+      <div id="boxCardsProje">
+        <CardsProject imgProject={sAcessi} altImg='Landing Page acessibilidade' nameProject='Landing Page acessibilidade' descriProject='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at consectetur ea molestias ad reprehenderit odit veniam consequuntur. At eveniet quas officia nesciunt ipsam eos, quasi amet ullam quis consequatur.' linkPro='https://lading-page-acessibilidade.vercel.app/'/>
+        <CardsProject imgProject={burger} altImg='Landing Page Hamburger' nameProject='Landing Page Hamburgueria' descriProject='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at consectetur ea molestias ad reprehenderit odit veniam consequuntur. At eveniet quas officia nesciunt ipsam eos, quasi amet ullam quis consequatur.' linkPro=''/>
+        <CardsProject imgProject={apiClima} altImg='Api de Clima' nameProject='Api Clima' descriProject='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at consectetur ea molestias ad reprehenderit odit veniam consequuntur. At eveniet quas officia nesciunt ipsam eos, quasi amet ullam quis consequatur.' linkPro='https://apiclima-three.vercel.app/'/>
+        <CardsProject imgProject={gerador} altImg='Gerador de senhas' nameProject='Gerador de Senhas' descriProject='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at consectetur ea molestias ad reprehenderit odit veniam consequuntur. At eveniet quas officia nesciunt ipsam eos, quasi amet ullam quis consequatur.' linkPro='https://gerador-senhas-2qovjqoft-caiosantos00.vercel.app/'/>
+        <CardsProject imgProject={quizImg} altImg='Quiz de Javascript' nameProject='Quiz Javascript' descriProject='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at consectetur ea molestias ad reprehenderit odit veniam consequuntur. At eveniet quas officia nesciunt ipsam eos, quasi amet ullam quis consequatur.' linkPro='https://quiz-javascript-mauve.vercel.app/'/>
+        <CardsProject imgProject={cardapio} altImg='Cardápio de hamburgueria' nameProject='Cardápio' descriProject='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at consectetur ea molestias ad reprehenderit odit veniam consequuntur. At eveniet quas officia nesciunt ipsam eos, quasi amet ullam quis consequatur.' linkPro='https://cardapio-hamburgueria.vercel.app/'/>
+        <div id="divBreve"><span className="nameProject">Em breve novos projetos...</span></div>
+      </div>
     )
   }
   
--- src/Componentes/Sobre/CardsTech/index.jsx
+++ src/Componentes/Sobre/CardsTech/index.jsx
@@ -2,6 +2,7 @@ export default function CardsTech({svgTech, nameTech}) {
     return (
         <>
         <div className="cardsTech">
+          <div className="nameTech">{nameTech}</div>
           <div className='svgTech'>{svgTech}</div>
         </div>
         </>
